4e066a49148e4aa515556c949d6e4a04475f4820
Kevin Koch <kevin.koch23@d155.org>
2022-09-15 08:11:09 -0500

    initial commit

--- CafeApp/ViewController.swift
+++ CafeApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var cartOutput: UILabel!
+    @IBOutlet weak var totalOutput: UILabel!
+    @IBOutlet weak var itemInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var total : Double = 0
+    var items = ["Coffee" : 6.49, "Bagel" : 3.99, "Muffin" : 2.99, "Cocoa" : 6.99, "Sandwich" : 8.49, "Tea" : 4.99]
+    @IBAction func addCart(_ sender: UIButton) {
+        if let text = itemInput.text, let result = items[text] {
+            cartOutput.text =
+            "\(cartOutput.text!)\n\(itemInput.text!) \(result)"
+            total = total + result
+            totalOutput.text = "Total: \n $\(total)"
+        else itemInput.text = "Not an option!"
+        }
+    }
+    
 }
 
