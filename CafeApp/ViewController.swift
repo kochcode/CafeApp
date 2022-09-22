05eecd6ec28028feff2f179b217822004068bbec
Kevin Koch <kevin.koch23@d155.org>
2022-09-22 08:10:53 -0500

    add button admin area

--- CafeApp/ViewController.swift
+++ CafeApp/ViewController.swift
@@ -11,9 +11,14 @@ class ViewController: UIViewController {
 
     
     
-    @IBOutlet weak var cartOutput: UILabel!
-    @IBOutlet weak var totalOutput: UILabel!
+    
+    @IBOutlet weak var listOutput: UITextView!
+    @IBOutlet weak var cartOutput: UITextView!
+    @IBOutlet weak var totalOutput: UITextView!
     @IBOutlet weak var itemInput: UITextField!
+    @IBOutlet weak var itemInputDos: UITextField!
+    @IBOutlet weak var priceInput: UITextField!
+    @IBOutlet weak var passWord: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +26,33 @@ class ViewController: UIViewController {
     }
     var total : Double = 0
     var items = ["Coffee" : 6.49, "Bagel" : 3.99, "Muffin" : 2.99, "Cocoa" : 6.99, "Sandwich" : 8.49, "Tea" : 4.99]
+    
     @IBAction func addCart(_ sender: UIButton) {
         if let text = itemInput.text, let result = items[text] {
             cartOutput.text =
             "\(cartOutput.text!)\n\(itemInput.text!) \(result)"
             total = total + result
-            totalOutput.text = "Total: \n $\(total)"
-        else itemInput.text = "Not an option!"
+            totalOutput.text = "Total: \n $\(round(1000*total)/1000)"
+        }
+        else{
+            itemInput.text = "Not an item!"
         }
     }
+    @IBAction func addButton(_ sender: UIButton) {
+        if passWord.text == "giveadmin"{
+            items[itemInputDos.text!] = Double(priceInput.text!)!
+            listOutput.text = "\(listOutput.text!)\n \(itemInputDos.text!)- $\(priceInput.text!)"
+        }
+    }
+    @IBAction func removeButton(_ sender: UIButton) {
+        if passWord.text == "giveadmin"{
+            items.removeValue(forKey: itemInputDos.text!)
+            
+        }
+    }
+    func clearMenu(){
+    }
+    
     
 }
 
