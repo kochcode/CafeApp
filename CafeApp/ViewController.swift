5e33c26d0c0bce5d6e6a7f2f9aeb26f8cca330d1
Kevin Koch <kevin.koch23@d155.org>
2022-09-26 08:52:01 -0500

    sort start

--- CafeApp/ViewController.swift
+++ CafeApp/ViewController.swift
@@ -23,10 +23,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        restartMenu()
     }
     var total : Double = 0
     var items = ["Coffee" : 6.49, "Bagel" : 3.99, "Muffin" : 2.99, "Cocoa" : 6.99, "Sandwich" : 8.49, "Tea" : 4.99]
-    
+    var sortCostItems = ["Coffee" : 6.49]
     @IBAction func addCart(_ sender: UIButton) {
         if let text = itemInput.text, let result = items[text] {
             cartOutput.text =
@@ -41,16 +42,27 @@ class ViewController: UIViewController {
     @IBAction func addButton(_ sender: UIButton) {
         if passWord.text == "giveadmin"{
             items[itemInputDos.text!] = Double(priceInput.text!)!
-            listOutput.text = "\(listOutput.text!)\n \(itemInputDos.text!)- $\(priceInput.text!)"
+            restartMenu()
         }
     }
     @IBAction func removeButton(_ sender: UIButton) {
         if passWord.text == "giveadmin"{
             items.removeValue(forKey: itemInputDos.text!)
-            
+            restartMenu()
         }
     }
-    func clearMenu(){
+    @IBAction func sortCost(_ sender: UIButton) {
+        let sortCostItems = items.sorted(by: <)
+        listOutput.text = "Scroll for more"
+        for (key,value) in sortCostItems{
+            listOutput.text = listOutput.text + "\n\(key)- $\(value)"
+        }
+    }
+    func restartMenu(){
+        listOutput.text = "Scroll for more"
+        for (key,value) in items{
+            listOutput.text = listOutput.text + "\n\(key)- $\(value)"
+        }
     }
     
     
